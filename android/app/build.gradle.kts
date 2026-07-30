8b8e5b24a606c9fb6fbe5b025ab76628752e0d73
mo1749 <13830482750@163.com>
2026-07-31 03:16:15 +0800

    release: v1.3.0 - 新增安卓版客户端

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.momusic.android"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 3
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
